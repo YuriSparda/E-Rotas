27bfd56bdc4f00a56a7327264b933a6f0b4770df
YuriSparda <95309127+YuriSparda@users.noreply.github.com>
2022-10-14 10:50:29 -0300

    update 1 date 14/10/2022

--- src/main/java/com/entra21tcc/ERotas/ERotasApplication.java
+++ src/main/java/com/entra21tcc/ERotas/ERotasApplication.java
@@ -1,7 +1,12 @@
 package com.entra21tcc.ERotas;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import com.entra21tcc.ERotas.entities.Client;
+import com.entra21tcc.ERotas.repositories.ClientRepository;
 
 @SpringBootApplication
 public class ERotasApplication {
@@ -10,4 +15,14 @@ public class ERotasApplication {
 		SpringApplication.run(ERotasApplication.class, args);
 	}
 
+	@Bean
+	public CommandLineRunner demo(ClientRepository ClientRep) {
+		return (args) -> {
+			ClientRep.save(new Client(null, "Marcos", null, null, null, null));
+			ClientRep.save(new Client(null, "Pedro", null, null, null, null));
+			ClientRep.save(new Client(null, "Tiago", null, null, null, null));
+			ClientRep.save(new Client(null, "Yurii", null, null, null, null));
+			ClientRep.save(new Client(null, "Wellington", null, null, null, null));
+		};
+	}
 }
---
+++ src/main/java/com/entra21tcc/ERotas/controllers/ClientController.java
@@ -0,0 +1,78 @@
+package com.entra21tcc.ERotas.controllers;
+
+import java.net.URI;
+import java.util.List;
+
+import javax.validation.Valid;
+
+import org.hibernate.ObjectNotFoundException;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+import com.entra21tcc.ERotas.entities.Client;
+import com.entra21tcc.ERotas.repositories.ClientRepository;
+import com.entra21tcc.ERotas.services.ClientServices;
+
+@RestController
+@RequestMapping(value = "/Clients")
+public class ClientController {
+
+    @Autowired
+    private ClientRepository cRepository;
+
+    @Autowired
+    private ClientServices cServices;
+
+    @GetMapping
+    public ResponseEntity<List<Client>> findAll() {
+        List<Client> clientList = cServices.findAll();
+        return ResponseEntity.ok().body(clientList);
+    }
+
+    @GetMapping(value = "/{id}")
+    public ResponseEntity<Client> findById(@PathVariable Integer id) {
+        Client clientList = cServices.findById(id);
+        return ResponseEntity.ok().body(clientList);
+    }
+
+    @PostMapping(value = "/newUser")
+    public ResponseEntity<Client> insClient(@Valid @RequestBody Client pClient) {
+        pClient.setIdClient(null);
+        cRepository.save(pClient);
+        URI yURI = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(pClient.getIdClient())
+                .toUri();
+        return ResponseEntity.created(yURI).body(pClient);
+    }
+
+    @PutMapping(value = "/editUser")
+    public ResponseEntity<Client> updClient(@Valid @PathVariable Integer id, @RequestBody Client pClient) {
+        Client atualClient = cRepository.findById(id)
+                .orElseThrow(
+                        () -> new ObjectNotFoundException(id, " user is not fond"));
+        atualClient.setName(pClient.getName());
+        atualClient.setNumber(pClient.getNumber());
+        atualClient.setMail(pClient.getMail());
+        atualClient.setCpf(pClient.getCpf());
+        // address
+        atualClient.setCep(pClient.getCep());
+        atualClient.setRua(pClient.getRua());
+        atualClient.setComplemento(pClient.getComplemento());
+        atualClient.setN_Casa(pClient.getN_Casa());
+        atualClient.setCidade(pClient.getCidade());
+        atualClient.setEstado(pClient.getEstado());
+        atualClient.setBairro(pClient.getBairro());
+        // end address
+        cRepository.save(atualClient);
+        return ResponseEntity.ok().body(atualClient);
+
+    }
+
+}
